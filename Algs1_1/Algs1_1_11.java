36895415772fe61e53fe5b790168f0983528fb4e
ShiraiYuu <shiraiyuu@163.com>
2021-03-02 12:55:59 +0800

    Amend: Algs1_1_11

--- Algs1_1/Algs1_1_11.java
+++ Algs1_1/Algs1_1_11.java
@@ -1,24 +1,33 @@
-package com.algorithms.shiraiyuu;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
+/**
+    Exercise 1.1.11 on P56(ENG), P34(CN)
+        To be clear, hyphen - is used for false value output.
+ */
 
 public class Algs1_1_11
 {
     public static void main(String[] args)
     {
-        ;
+        boolean[][] bArr = {{true,false,true,false,true},{false,true,false,true},{true,false,true},{false,true},{true}};
+        System.out.println(boolToString(bArr));
     }
 
     public static String boolToString(boolean[][] arr)
     {
+        // Print table headers
         var str = new StringBuilder();
         str.append("  ");
-        for(int i=0;i<rowMaxLen(arr);i++) str.append(i+" "); str.append("\n");
+        for(int i=0;i<rowMaxLen(arr);i++)
+        {
+            str.append(i);
+            str.append(" ");
+        }
+        str.append("\n");
+
         int rowNumber = 0;
         for(boolean[] row : arr)
         {
-            str.append(rowNumber+" "); rowNumber++;
+            str.append(rowNumber); str.append(" "); rowNumber++;
+
             for(boolean b : row) str.append(b?"* ":"- ");
             str.append("\n");
         }
@@ -27,8 +36,8 @@ public class Algs1_1_11
 
     private static int rowMaxLen(boolean[][] arr)
     {
-        int max=0;
-        for(int i=0;i<arr.length-1;i++) max = Math.max(arr[i].length,arr[i+1].length);
-        return max;
+        int maxV =0;
+        for (boolean[] booleans : arr) maxV = Math.max(maxV, booleans.length);
+        return maxV;
     }
 }
