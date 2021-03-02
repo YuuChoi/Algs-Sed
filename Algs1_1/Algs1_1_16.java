f631f73ea784104da07be7af846822d236639671
ShiraiYuu <shiraiyuu@163.com>
2021-03-02 19:03:20 +0800

    Answer for 1.1.16

---
+++ Algs1_1/Algs1_1_16.java
@@ -0,0 +1,20 @@
+/*
+    Exercise 1.1.16
+ */
+
+import java.util.Arrays;
+
+public class Algs1_1_16
+{
+    public static void main(String[] args)
+    {
+        System.out.print(exR1(6));
+    }
+
+    public static String exR1(int n)
+    {
+        if(n<=0) return "";
+        return exR1(n-3) + n + exR1(n-2) + n;
+    }
+
+}
